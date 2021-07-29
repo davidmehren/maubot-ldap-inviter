2e824046eb901f53e7e2cd41cc31e3fd9c00fb3c
David Mehren <git@herrmehren.de>
2021-07-29 15:52:51 +0200

    Add retrieval of users in a LDAP group

--- ldap-inviter.py
+++ ldap-inviter.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Type, Optional, TypedDict
+from typing import Type, Optional, TypedDict, Mapping, List
 
 from ldap.ldapobject import SimpleLDAPObject
 from maubot.handlers import command
@@ -15,11 +15,34 @@ from maubot import Plugin, MessageEvent
 
 
 class UserConfig(TypedDict):
+    power_level: Optional[int]
+
+
+UserInfoMap = Mapping[str, UserConfig]
+
+
+class LDAPMemberConfig(TypedDict):
+    ldap_group: str
+    power_level: Optional[int]
+
+
+class MemberConfig(TypedDict):
     mxid: str
     power_level: Optional[int]
 
 
-class Config(BaseProxyConfig):
+class SyncRoomConfig(TypedDict):
+    alias: str
+    visibility: str
+    name: str
+    ldap_members: List[LDAPMemberConfig]
+    members: List[MemberConfig]
+
+
+class LDAPInviterConfig(BaseProxyConfig):
+    sync_rooms: List[SyncRoomConfig]
+    admin_users: List[str]
+
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("sync_rooms")
         helper.copy("admin_users")
@@ -29,6 +52,7 @@ class Config(BaseProxyConfig):
 class LDAPInviterBot(Plugin):
     room_methods = None
     event_methods = None
+    config: LDAPInviterConfig
 
     async def start(self) -> None:
         await super().start()
@@ -38,7 +62,7 @@ class LDAPInviterBot(Plugin):
 
     @classmethod
     def get_config_class(cls) -> Type[BaseProxyConfig]:
-        return Config
+        return LDAPInviterConfig
 
     @staticmethod
     def state_events_to_member_list(state_events: [StateEvent]):
@@ -84,24 +108,23 @@ class LDAPInviterBot(Plugin):
             await evt.respond(f'Found room: {room.room_id}')
             return room.room_id
 
-    async def ensure_room_invitees(self, evt: MessageEvent, room_id: RoomID, users: [UserConfig]):
+    async def ensure_room_invitees(self, evt: MessageEvent, room_id: RoomID, user_info_map: UserInfoMap):
         room_member_events = await self.event_methods.get_members(room_id)
         room_members, room_invitees = self.state_events_to_member_list(room_member_events)
         await evt.respond(f'Room {room_id} has members:{str(room_members)}')
         await evt.respond(f'Room {room_id} has invitees:{str(room_invitees)}')
-        for user_id in users:
-            if user_id['mxid'] not in room_members and user_id['mxid'] not in room_invitees:
-                await evt.respond(f'User {user_id["mxid"]} not invited or in the room, inviting...')
-                await self.room_methods.invite_user(room_id, user_id['mxid'])
+        for mxid in user_info_map:
+            if mxid not in room_members and mxid not in room_invitees:
+                await evt.respond(f'User {mxid} not invited or in the room, inviting...')
+                await self.room_methods.invite_user(room_id, mxid)
         await evt.respond(f'Successfully ensured invitees for {room_id}')
 
-    async def ensure_room_power_levels(self, evt: MessageEvent, room_id: RoomID, users: [UserConfig]):
+    async def ensure_room_power_levels(self, evt: MessageEvent, room_id: RoomID, user_info_map: UserInfoMap):
         current_state = await self.room_methods.get_state_event(room_id, EventType.ROOM_POWER_LEVELS)
         current_power_levels: dict[UserID, int] = current_state['users']
         await evt.respond(f'Current power levels: {str(current_power_levels)}')
-        user: UserConfig
-        for user in users:
-            current_power_levels[UserID(user['mxid'])] = user['power_level']
+        for mxid in user_info_map:
+            current_power_levels[UserID(mxid)] = user_info_map[mxid]['power_level']
         await self.room_methods.send_state_event(room_id, EventType.ROOM_POWER_LEVELS,
                                                  PowerLevelStateEventContent(users=current_power_levels))
         await evt.respond(f'Successfully ensured power levels')
@@ -112,6 +135,14 @@ class LDAPInviterBot(Plugin):
             raise Exception(f'Room alias "{alias}" includes a placeholder, but no argument was provided')
         return alias.replace('<1>', arg1)
 
+    @staticmethod
+    def to_user_info_map(member_config: [MemberConfig]) -> UserInfoMap:
+        user_info_map = {}
+        user: dict
+        for user in member_config:
+            user_info_map[user['mxid']] = UserConfig(power_level=user.get('power_level', 0))
+        return user_info_map
+
     async def sync_room(self, evt: MessageEvent, room, arg1: str):
         alias = room['alias']
         alias = self.template_room_alias(alias, arg1)
@@ -121,9 +152,9 @@ class LDAPInviterBot(Plugin):
         # Ensure room has the correct name
         await self.ensure_room_name(evt, room_id, room['name'])
         # Ensure hardcoded users are invited
-        await self.ensure_room_invitees(evt, room_id, room['members'])
+        await self.ensure_room_invitees(evt, room_id, self.to_user_info_map(room['members']))
         # Ensure users have correct power levels
-        await self.ensure_room_power_levels(evt, room_id, room['members'])
+        await self.ensure_room_power_levels(evt, room_id, self.to_user_info_map(room['members']))
         # Ensure room is (in) visible in Room Directory
         await self.ensure_room_visibility(evt, room_id, room['visibility'])
         await evt.respond(f'Successfully synced room.')
@@ -132,12 +163,17 @@ class LDAPInviterBot(Plugin):
         for room in rooms:
             await self.sync_room(evt, room, arg1)
 
-    async def get_matrix_users_of_ldap_group(self, evt: MessageEvent, con: SimpleLDAPObject, ldap_group: str) -> [str]:
+    async def get_matrix_users_of_ldap_group(self, evt: MessageEvent, con: SimpleLDAPObject,
+                                             ldap_group: str, power_level: int) -> UserInfoMap:
         import ldap
         await evt.respond(f'Getting users for LDAP group {ldap_group}...')
         ldap_filter = f"(&{self.config['ldap']['user_filter']}(memberOf={ldap_group}))"
         group_members = con.search_s(self.config['ldap']['base_dn'], ldap.SCOPE_SUBTREE, ldap_filter, ['uid'])
-        return list(map(lambda member: member[1]['uid'][0].decode("utf-8"), group_members))
+        mxids = map(lambda member: member[1]['uid'][0].decode("utf-8"), group_members)
+        user_map = {}
+        for mxid in mxids:
+            user_map[mxid] = UserConfig(power_level=power_level)
+        return user_map
 
     @command.new(name='ldap-sync')
     @command.argument("arg1", "Argument 1", pass_raw=True, required=False)
@@ -153,6 +189,10 @@ class LDAPInviterBot(Plugin):
             await asyncio.sleep(5)
             await evt.respond(f'Encountered fatal error: {e}')
 
+    @command.new(name='debug-map')
+    async def ldap_check(self, evt: MessageEvent):
+        await evt.respond(f'User map: {self.to_user_info_map(self.config["sync_rooms"][0]["members"])}')
+
     @command.new(name='ldap-check')
     async def ldap_check(self, evt: MessageEvent):
         await evt.respond("Checking LDAP connection...")
@@ -162,9 +202,11 @@ class LDAPInviterBot(Plugin):
             con = ldap.initialize(self.config['ldap']['uri'])
             con.simple_bind_s(self.config['ldap']['connect_dn'], self.config['ldap']['connect_password'])
             await evt.respond(f'Successfully connected. I am: {con.whoami_s()}')
+            room: SyncRoomConfig
             for room in self.config['sync_rooms']:
                 for ldap_group in room['ldap_members']:
-                    uids = await self.get_matrix_users_of_ldap_group(evt, con, ldap_group['ldap_group'])
+                    uids = await self.get_matrix_users_of_ldap_group(evt, con, ldap_group['ldap_group'],
+                                                                     ldap_group['power_level'])
                     await evt.respond(f'Members of group {ldap_group}: {uids}')
             con.unbind_s()
         except Exception as e:
