db7f683d176c9558f126ef65de887d466c1d3ca1
David Mehren <git@herrmehren.de>
2021-07-30 09:58:36 +0200

    Run black

--- inviter/bot.py
+++ inviter/bot.py
@@ -33,42 +33,50 @@ class LDAPInviterBot(Plugin):
     @staticmethod
     def template_room_alias(alias: str, arg1: str) -> str:
         if "<1>" in alias and arg1 == "":
-            raise Exception(f'Room alias "{alias}" includes a placeholder, but no argument was provided')
-        return alias.replace('<1>', arg1)
+            raise Exception(
+                f'Room alias "{alias}" includes a placeholder, but no argument was provided'
+            )
+        return alias.replace("<1>", arg1)
 
     @staticmethod
     def to_user_info_map(member_config: [MemberConfig]) -> UserInfoMap:
         user_info_map = {}
         user: dict
         for user in member_config:
-            user_info_map[user['mxid']] = UserConfig(power_level=user.get('power_level', 0))
+            user_info_map[user["mxid"]] = UserConfig(
+                power_level=user.get("power_level", 0)
+            )
         return user_info_map
 
     async def sync_room(self, evt: MessageEvent, room, arg1: str):
-        alias = room['alias']
+        alias = room["alias"]
         alias = self.template_room_alias(alias, arg1)
-        await evt.respond(f'Syncing room: {alias}')
+        await evt.respond(f"Syncing room: {alias}")
         # Ensure room exists
         room_id = await self.matrix_utils.ensure_room_with_alias(evt, alias)
         # Ensure room has the correct name
-        await self.matrix_utils.ensure_room_name(evt, room_id, room['name'])
+        await self.matrix_utils.ensure_room_name(evt, room_id, room["name"])
         # Ensure hardcoded users are invited
-        await self.matrix_utils.ensure_room_invitees(evt, room_id, self.to_user_info_map(room['members']))
+        await self.matrix_utils.ensure_room_invitees(
+            evt, room_id, self.to_user_info_map(room["members"])
+        )
         # Ensure users have correct power levels
-        await self.matrix_utils.ensure_room_power_levels(evt, room_id, self.to_user_info_map(room['members']))
+        await self.matrix_utils.ensure_room_power_levels(
+            evt, room_id, self.to_user_info_map(room["members"])
+        )
         # Ensure room is (in) visible in Room Directory
-        await self.matrix_utils.ensure_room_visibility(evt, room_id, room['visibility'])
-        await evt.respond(f'Successfully synced room.')
+        await self.matrix_utils.ensure_room_visibility(evt, room_id, room["visibility"])
+        await evt.respond(f"Successfully synced room.")
 
     async def sync_rooms(self, evt, rooms, arg1: str):
         for room in rooms:
             await self.sync_room(evt, room, arg1)
 
-    @command.new(name='ldap-sync')
+    @command.new(name="ldap-sync")
     @command.argument("arg1", "Argument 1", pass_raw=True, required=False)
     async def ldap_sync(self, evt: MessageEvent, arg1: str) -> None:
-        if evt.sender not in self.config['admin_users']:
-            await evt.respond('You are not allowed to run a sync.')
+        if evt.sender not in self.config["admin_users"]:
+            await evt.respond("You are not allowed to run a sync.")
             return None
         await evt.respond(f'Starting sync. Arg1: "{arg1}"')
         try:
@@ -76,26 +84,36 @@ class LDAPInviterBot(Plugin):
         except Exception as e:
             # Wait a bit to hopefully clear too many requests
             await asyncio.sleep(5)
-            await evt.respond(f'Encountered fatal error: {e}')
+            await evt.respond(f"Encountered fatal error: {e}")
 
-    @command.new(name='debug-map')
+    @command.new(name="debug-map")
     async def ldap_check(self, evt: MessageEvent):
-        await evt.respond(f'User map: {self.to_user_info_map(self.config["sync_rooms"][0]["members"])}')
+        await evt.respond(
+            f'User map: {self.to_user_info_map(self.config["sync_rooms"][0]["members"])}'
+        )
 
-    @command.new(name='ldap-check')
+    @command.new(name="ldap-check")
     async def ldap_check(self, evt: MessageEvent):
         await evt.respond("Checking LDAP connection...")
         try:
             import ldap
+
             # Create LDAP connection
-            con = ldap.initialize(self.config['ldap']['uri'])
-            con.simple_bind_s(self.config['ldap']['connect_dn'], self.config['ldap']['connect_password'])
-            await evt.respond(f'Successfully connected. I am `{con.whoami_s()}`')
+            con = ldap.initialize(self.config["ldap"]["uri"])
+            con.simple_bind_s(
+                self.config["ldap"]["connect_dn"],
+                self.config["ldap"]["connect_password"],
+            )
+            await evt.respond(f"Successfully connected. I am `{con.whoami_s()}`")
             room: SyncRoomConfig
-            for room in self.config['sync_rooms']:
-                uids = await get_all_matrix_users_of_sync_room(self.config, evt, con, room)
-                await evt.respond(f'Members of room `{room["alias"]}`:\n```\n{pformat(uids)}\n```')
+            for room in self.config["sync_rooms"]:
+                uids = await get_all_matrix_users_of_sync_room(
+                    self.config, evt, con, room
+                )
+                await evt.respond(
+                    f'Members of room `{room["alias"]}`:\n```\n{pformat(uids)}\n```'
+                )
             con.unbind_s()
         except Exception as e:
-            await evt.respond(f'Encountered fatal error: {e}')
+            await evt.respond(f"Encountered fatal error: {e}")
             raise e
--- inviter/ldap.py
+++ inviter/ldap.py
@@ -7,25 +7,34 @@ from .config import SyncRoomConfig
 from .matrix_utils import UserInfoMap, UserConfig
 
 
-async def get_matrix_users_of_ldap_group(config, evt: MessageEvent, con: SimpleLDAPObject,
-                                         ldap_group: str, power_level: int) -> UserInfoMap:
+async def get_matrix_users_of_ldap_group(
+    config, evt: MessageEvent, con: SimpleLDAPObject, ldap_group: str, power_level: int
+) -> UserInfoMap:
     import ldap
-    await evt.respond(f'Getting users for LDAP group `{ldap_group}`')
+
+    await evt.respond(f"Getting users for LDAP group `{ldap_group}`")
     ldap_filter = f"(&{config['ldap']['user_filter']}(memberOf={ldap_group}))"
-    group_members = con.search_s(config['ldap']['base_dn'], ldap.SCOPE_SUBTREE, ldap_filter, ['uid'])
-    mxids = map(lambda member: f'@{member[1]["uid"][0].decode("utf-8")}:{config["ldap"]["mxid_homeserver"]}',
-                group_members)
+    group_members = con.search_s(
+        config["ldap"]["base_dn"], ldap.SCOPE_SUBTREE, ldap_filter, ["uid"]
+    )
+    mxids = map(
+        lambda member: f'@{member[1]["uid"][0].decode("utf-8")}:{config["ldap"]["mxid_homeserver"]}',
+        group_members,
+    )
     user_map = {}
     for mxid in mxids:
         user_map[mxid] = UserConfig(power_level=power_level)
     return user_map
 
 
-async def get_all_matrix_users_of_sync_room(config, evt: MessageEvent, con: SimpleLDAPObject,
-                                            sync_room: SyncRoomConfig) -> UserInfoMap:
+async def get_all_matrix_users_of_sync_room(
+    config, evt: MessageEvent, con: SimpleLDAPObject, sync_room: SyncRoomConfig
+) -> UserInfoMap:
     user_info_map = {}
-    for ldap_config in sync_room['ldap_members']:
+    for ldap_config in sync_room["ldap_members"]:
         user_info_map.update(
-            await get_matrix_users_of_ldap_group(config, evt, con, ldap_config['ldap_group'],
-                                                 ldap_config['power_level']))
+            await get_matrix_users_of_ldap_group(
+                config, evt, con, ldap_config["ldap_group"], ldap_config["power_level"]
+            )
+        )
     return user_info_map
